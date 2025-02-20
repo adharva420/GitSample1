5a2446583109718bd94ad38ca1f7e8e4082f8254
adharva420 <adharva420@gmail.com>
2025-02-20 13:46:28 +0530

    New Code

--- src/test/java/org/sample/GitFirst.java
+++ src/test/java/org/sample/GitFirst.java
@@ -1,5 +1,17 @@
 package org.sample;
 
+import org.testng.annotations.Test;
+
 public class GitFirst {
+	@Test
+	public void test() throws Exception {
+		
+	}
+	@Test
+	private void test1() {
+		// TODO Auto-generated method stub
+
+	}
+	
 
 }
